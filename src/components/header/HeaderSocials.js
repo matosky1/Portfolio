8df772f636b381c12068ab06f98e6ca5b55b4f9e
Nzubechukwu Anthony Odinukwe <odinukweanthony@gmail.com>
2025-05-05 15:44:01 -0600

    Coded heaersocial and cta

---
+++ src/components/header/HeaderSocials.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+      <a href="https://www.linkedin.com/in/anthony-odinukwe/" target="_blank"><FaLinkedin /></a>
+      <a href="https://github.com/matosky1" target="_blank"><FaGithub /></a>
+      <a href="https://www.instagram.com/matosky_jnr/" target="_blank"><FaInstagram /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials  from './HeaderSocials'
 
 const header = () => {
   return (
@@ -8,6 +11,14 @@ const header = () => {
         <h5>Hello I'm</h5>
         <h1>Anthony Odinukwe Nzubechukwu</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocials />
+
+        <div className="me">
+            <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
        </div>
     </header>
   )
